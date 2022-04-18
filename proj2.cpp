b612122d6646db24ea709de9eeb5c8750c0a275e
kbrandon17 <kylebrandon17@gmail.com>
2022-04-17 23:53:30 -0700

    parameter checking

--- proj2.cpp
+++ proj2.cpp
@@ -9,13 +9,25 @@
 
 using std::string;
 
+void isNumber(char number[])
+{
+    for (int i = 0; number[i] != 0; i++)
+    {
+        if (!isdigit(number[i]))
+            printf("All non-filepath variables are not integers, exiting now!");
+            exit(1);
+    }
+}
+
 int main(int argc, char** argv) {
     char filePath[30];
     
-
     if (argc != 5) {
         printf("Incorrect number of arguments provided. Exiting now!\n");
         exit(1);
+    }
+    for (int i = 2; i < 5; i++) {
+        isNumber(argv[i]);
     }
 
     strcpy(filePath, argv[1]);
